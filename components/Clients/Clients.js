633cfa38faf1de9a236954a901e60773bd555793
tatoGi <workspace.tato@gmail.com>
2025-02-15 01:17:19 +0400

    Fix client image import path capitalization

--- components/Clients/Clients.js
+++ components/Clients/Clients.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./Clients.module.css";
-import client1 from "../../assets/img/client-1.png";
+import client1 from "../../assets/img/Client-1.png";
 
 const testimonials = [
   {
