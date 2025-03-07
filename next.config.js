4bff1fd155f4dd83725683edb3e59c597b4c5c71
tatoGi <workspace.tato@gmail.com>
2025-03-07 17:14:16 +0400

    Set default API_BASE_URL in next.config.js and add error handling for missing environment variable

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://test.ioka.ae/';
 
 if (!API_BASE_URL) {
   throw new Error('NEXT_PUBLIC_API_URL environment variable is not defined');
