5fa38e246b14f7f60216cae74e82b8be20733f4a
tatoGI <workspace.tato@gmail.com>
2025-03-11 19:51:23 +0400

    fix rental

--- components/Rental_Resale/Rental_Resale.js
+++ components/Rental_Resale/Rental_Resale.js
@@ -1,1 +1,12 @@
 import React from "react";
+
+
+const Rental_Resale = () => {
+    return (
+        <div>
+            <h1>Rental_Resale</h1>
+        </div>
+    );
+};
+
+export default Rental_Resale;
