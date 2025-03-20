c6edaca75c581ad4bf4167d8cc71494ae6554f56
tatoGI <workspace.tato@gmail.com>
2025-03-20 19:54:03 +0400

    `Refactored TeamSection component and updated global styles`

--- components/TeamSection/TeamSection.js
+++ components/TeamSection/TeamSection.js
@@ -1,49 +1,37 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import TeamImage from '../../assets/img/team-1.png'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import TeamImage from '../../assets/img/team-1.png';
 
 const TeamSection = () => {
   return (
     <div className="team-section">
-        <div className="container">
-            <div className="team-all-link">
-              <Link  href={"#"} className="team-all-link-touch">
-                Get in Touch
-              </Link>
-            </div>
-            <div className="team-box">
-              <Link href={'#'} className="team-item">
-                  <Image src={TeamImage} alt='member' />
-                  <div className="name">
-                    John Doe
-                  </div>
-                  <div className="experience">
-                    5+ Yeas Exprience
-                  </div>
-              </Link>
-              <Link href={'#'} className="team-item">
-                  <Image src={TeamImage} alt='member' />
-                  <div className="name">
-                    John Doe
-                  </div>
-                  <div className="experience">
-                    5+ Yeas Exprience
-                  </div>
-              </Link>
-              <Link href={'#'} className="team-item">
-                  <Image src={TeamImage} alt='member' />
-                  <div className="name">
-                    John Doe
-                  </div>
-                  <div className="experience">
-                    5+ Yeas Exprience
-                  </div>
-              </Link>
-            </div>
+      <div className="container">
+        <div className="team-all-link">
+          <Link href={"#"} className="team-all-link-touch">
+            ABOUT US
+          </Link>
         </div>
+        <div className="team-box">
+          <div className="team-item">
+            <Image src={TeamImage} alt='member' />
+            <div className="name">John Doe</div>
+            <div className="experience">5+ Years Experience</div>
+          </div>
+          <div className="team-item">
+            <Image src={TeamImage} alt='member' />
+            <div className="name">Max Musterman</div>
+            <div className="experience">5+ Years Experience</div>
+          </div>
+          <div className="team-item">
+            <Image src={TeamImage} alt='member' />
+            <div className="name">John Lenon</div>
+            <div className="experience">5+ Years Experience</div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TeamSection
+export default TeamSection;
