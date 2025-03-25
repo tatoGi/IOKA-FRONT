5301e06d4a21860f4e2d1703929a7358d67f219e
tatoGI <workspace.tato@gmail.com>
2025-03-25 23:54:04 +0400

    `Refactor NewsSection component to decode image URLs and fetch images from API URL`

--- components/NewsSection/NewsSection.js
+++ components/NewsSection/NewsSection.js
@@ -13,18 +13,23 @@ import LeftArrow from "../icons/LeftArrow";
 import Image from "next/image";
 
 const NewsSection = ({ sectionSevenData }) => {
-  console.log(sectionSevenData);
+  const decodeImageUrl = (url) => {
+    return decodeURIComponent(url);
+  };
   return (
     <div className="news-section">
       <div className="container">
         <div className="news-header">
           <div className="news-title">{sectionSevenData?.title}</div>
-          <Link href={sectionSevenData?.redirect_link || "#"} className="news-see-more">
+          <Link
+            href={sectionSevenData?.redirect_link || "#"}
+            className="news-see-more"
+          >
             SEE MORE
           </Link>
         </div>
         <div className="news-text">
-          {sectionSevenData?.subtitle}
+          {sectionSevenData?.additional_fields?.subtitle}
         </div>
         <div className="news-slider-s">
           <Swiper
@@ -37,45 +42,59 @@ const NewsSection = ({ sectionSevenData }) => {
             breakpoints={{
               320: {
                 slidesPerView: 1,
-                spaceBetween: 10,
+                spaceBetween: 10
               },
               575: {
                 slidesPerView: 1,
-                spaceBetween: 24,
+                spaceBetween: 24
               },
               640: {
                 slidesPerView: 2,
-                spaceBetween: 24,
+                spaceBetween: 24
               },
               1024: {
                 slidesPerView: 3,
-                spaceBetween: 24,
+                spaceBetween: 24
               },
               1560: {
                 slidesPerView: 4,
-                spaceBetween: 24,
-              },
+                spaceBetween: 24
+              }
             }}
           >
-            {sectionSevenData?.Recent_Articles?.map((article, index) => (
-              <SwiperSlide key={index}>
-                <div className="news-item">
-                  <div className="n-image">
-                    <Image src={article.image} alt="newsimage" />
-                  </div>
-                  <div className="n-content">
-                    <span>{article.Tips&Tricks}</span>
-                    <div className="n-text">
-                      {article.title}
+            {sectionSevenData?.additional_fields?.Recent_Articles?.map(
+              (article, index) => (
+                <SwiperSlide key={index}>
+                  <div className="news-item">
+                    <div className="n-image">
+                      <Image
+                        src={
+                          article.image
+                            ? `${
+                                process.env.NEXT_PUBLIC_API_URL
+                              }/storage/${decodeImageUrl(article.image)}`
+                            : baseimage
+                        }
+                        alt="newsimage"
+                        width={400}
+                        height={400}
+                      />
                     </div>
+                    <div className="n-content">
+                      <span></span>
+                      <div className="n-text">{article.title}</div>
+                    </div>
+                    <Link
+                      href={article.read_more || "#"}
+                      className="news-read-more"
+                    >
+                      Read More
+                      <LeftArrow />
+                    </Link>
                   </div>
-                  <Link href={article.read_more || "#"} className="news-read-more">
-                    Read More
-                    <LeftArrow />
-                  </Link>
-                </div>
-              </SwiperSlide>
-            ))}
+                </SwiperSlide>
+              )
+            )}
           </Swiper>
         </div>
       </div>
