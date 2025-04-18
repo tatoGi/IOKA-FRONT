ce8694fc287e8c92489b307661cbafcbeb1ec2d6
tatoGI <workspace.tato@gmail.com>
2025-04-18 16:04:21 +0400

    meta viewport

--- components/Meta/Meta.js
+++ components/Meta/Meta.js
@@ -28,7 +28,8 @@ const Meta = ({
   return (
     <Head>
       {/* Basic Meta Tags */}
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
       <link rel="icon" href="/favicon.ico" />
