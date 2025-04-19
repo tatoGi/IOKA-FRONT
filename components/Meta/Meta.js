97420ab5aff32ff53084c213d5d780723275cc93
tatoGI <workspace.tato@gmail.com>
2025-04-19 16:37:36 +0400

    Update Meta component viewport settings and add iOS-specific styles to RentalList CSS
    - Modified the viewport meta tag in Meta.js to prevent user scaling for better mobile experience.
    - Added iOS-specific styles in RentalList.module.css to enhance compatibility with touch interactions.

--- components/Meta/Meta.js
+++ components/Meta/Meta.js
@@ -28,7 +28,7 @@ const Meta = ({
   return (
     <Head>
       {/* Basic Meta Tags */}
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
